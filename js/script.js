4cf88850e55373befa4773a6d3906b0021be846c
Methuselah <dstengac@gmail.com>
2024-06-23 22:38:58 +0200

    tabs

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-document.addEventListener("DOMcontentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     const tabs  = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent"),
     tabsParent = document.querySelector(".tabheader__items");
 
     function hideTabContent() {
         tabsContent.forEach(item => {
-             item.style.display = "none";
+            item.style.display = "none";
+            // item.classList.add("hide");
+            // item.classList.remove("show");
         })
 
         tabs.forEach(item => {
@@ -13,10 +15,25 @@ document.addEventListener("DOMcontentLoaded", () => {
         })
     }
 
-    function showTabContent(i) {
+    function showTabContent(i = 0) {
         tabsContent[i].style.display = "block";
+        // tabsContent[i].classList.add("show");
+        // tabsContent[i].classList.remove("hide");
         tabs[i].classList.add("tabheader__item_active");
     }
     hideTabContent();
     showTabContent(0);
+
+    tabsParent.addEventListener("click", (e) => {
+        const target = e.target;
+
+        if(target && target.classList.contains("tabheader__item")) {
+             tabs.forEach((item, i) => {
+                if(target == item) {
+                    hideTabContent();
+                    showTabContent(i);  
+                }
+             })
+        }
+    })
 })
