68d050eb4d28253ccde7bf038d72e0783d56f788
Methuselah <dstengac@gmail.com>
2024-06-26 23:15:53 +0200

    factorial

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ fetch(url)
   .then(response => response.json()) 
   .then(data => console.log(data))
   .catch(error => console.error('Ошибка при выполнении запроса:', error));
+
+  function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+// Пример использования функции
+console.log(factorial(5)); // Выведет в консоль результат: 120
