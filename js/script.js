ae1e3b725417c15d9d710cc2126e96782d36c484
Methuselah <dstengac@gmail.com>
2024-06-30 23:26:07 +0200

    fetch

--- js/script.js
+++ js/script.js
@@ -4,17 +4,3 @@ fetch(url)
   .then(response => response.json()) 
   .then(data => console.log(data))
   .catch(error => console.error('Ошибка при выполнении запроса:', error));
-
-
-// Функция для изменения цвета фона
-function changeBackgroundColor() {
-  // Случайный цвет
-  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-  document.body.style.backgroundColor = randomColor;
-}
-
-// Добавление обработчика событий для кнопки
-document.getElementById('colorButton').addEventListener('click', changeBackgroundColor);
-
-// Пример HTML кода для кнопки:
-// <button id="colorButton">Change Background Color</button>
