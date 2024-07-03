a6ffa3811715b7764b4baf9a45accedd5d674d36
Methuselah <dstengac@gmail.com>
2024-07-03 18:33:53 +0200

    Added functionality to append list items via button click

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ fetch(url)
   .then(response => response.json()) 
   .then(data => console.log(data))
   .catch(error => console.error('Ошибка при выполнении запроса:', error));
+
+  function addListItem() {
+    const listItem = document.createElement('li');
+    const inputText = document.getElementById('itemInput').value;
+    listItem.textContent = inputText;
+
+    document.getElementById('itemList').appendChild(listItem);
+    document.getElementById('itemInput').value = ''; // Очистить поле ввода
+}
+
+// Добавление обработчика событий для кнопки
+document.getElementById('addItemButton').addEventListener('click', addListItem);
